258a9d0681587193be45a361ab448237c595b6d4
Dev Chitara <devchitara2003@gmail.com>
2023-10-23 23:07:56 +0530

    completed some fuctionality of user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    user_name = models.CharField(max_length=20)
+    role_title = models.CharField(max_length=20)
+    department = models.CharField(max_length=20)
+    location = models.FloatField()
+    emp_no = models.IntegerField()
+
+    
